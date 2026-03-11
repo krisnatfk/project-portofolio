f44afabd027c19705f77278a4b546a2d71bde763
krisnatfk <k1215na77@gmail.com>
2026-03-11 23:52:30 +0700

    fix

--- common/constants/projectUrls.ts
+++ common/constants/projectUrls.ts
@@ -1,4 +1,4 @@
 export const PROJECT_CUSTOM_URLS: Record<string, string> = {
     quickshow: "https://cleint-plum.vercel.app/",
-    "web-ayam-fillet": "https://e-commerce-ayfh0y53x-krisnagamingchh-8455s-projects.vercel.app/",
+    "e-commerce": "https://e-commerce-ayfh0y53x-krisnagamingchh-8455s-projects.vercel.app/",
 };
